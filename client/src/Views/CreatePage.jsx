1b892344c80598923f5ba5c1e9ed0c7dcbd2c545
Dan <121922311+Dmatter03@users.noreply.github.com>
2023-03-26 20:26:54 -0400

    added useState

--- client/src/Views/CreatePage.jsx
+++ client/src/Views/CreatePage.jsx
@@ -2,6 +2,7 @@
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { useParams, useNavigate } from "react-router-dom";
+import React, { useState } from 'react'
 
 import PlayerForm from '../Components/PlayerForm';
 
